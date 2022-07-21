d97d14af2e8eeb0091e7f678797a262048727d4e
RMo <morici23@gmail.com>
2022-07-20 22:00:13 -0400

    Cleanup NewTablature.js

--- src/pages/NewTablature/NewTablature.js
+++ src/pages/NewTablature/NewTablature.js
@@ -1,20 +1,17 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useContext } from "react";
 import { CircularProgress } from '@mui/material';
 import { create } from "../../services/tablatureServices";
 import { UserContext } from '../../App';
-import { Navigate, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { getIdTokenFromUser } from "../../utils/userUtils";
 
 
 const NewTablature = () => {
-    const [tablatureId, setTablatureId] = useState(null);
     const { user } = useContext(UserContext);
     const navigate = useNavigate()
 
     useEffect(()=> {
-        // // TODO Send a fetch request to the server
-        // // TODO SHow spinny while waiting for response
-        // // TODO When successsful response is received, reroute to /tablature/:id
+
         if(user) {
             const username = user.username;
             const idToken = getIdTokenFromUser(user);
@@ -25,9 +22,7 @@ const NewTablature = () => {
         }
     }, [user]);
 
-    return (
-        <CircularProgress />
-    );
+    return ( <CircularProgress /> );
 }
  
 export default NewTablature;
