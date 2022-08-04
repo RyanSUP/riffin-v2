f1ab22aa32585b80a801f87d6953339255b88718
RMo <morici23@gmail.com>
2022-08-03 20:47:25 -0400

    Determine if the profile page is the current user's page or someone elses page. Currently this just shows a text message, but the information will be needed to determine which tablature to show. If the current user is on their own page then usersTablature and usersFavorite tablature from state will be shown. Otherwise a request needs to go out to get that profile's public tablature.

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,7 +1,24 @@
+import { useContext } from "react"
+import { useParams } from "react-router-dom"
+import { UserContext } from '../../App'
+
+// TODO ---
+// If user is the same as :id, then show tablature from state (usersTablature and usersFavoriteTablature). Otherwise make a request to get all of the users public tablature.
+// Show loading wheel while user is undefined.
+// TODO ---
+
 const Profile = () => {
+    const { user } = useContext(UserContext)
+    const { id } = useParams()
+    
     return (
         <>
-            This is a profile page
+            {(user?.username === id)
+                ?
+                    <p>This is the current user's page!</p>
+                :
+                    <p>This is someone elses page!</p>
+            }
         </>
     );
 }
