f0ebe4f278e0608a64a920312eed7d92068118d6
RMo <morici23@gmail.com>
2022-08-09 08:55:29 -0400

    Tidy App.js and Fix issue that caused constant requests from the profile service.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,39 @@
+// Services
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import { createContext } from "react";
 import { CognitoUser, AuthenticationDetails } from "amazon-cognito-identity-js";
 import { useEffect, useState, useCallback } from "react";
-import TablatureEditorPLUS from './pages/TablatureEditorPLUS/TablatureEditorPLUS';
 import UserPool from "./utils/UserPool";
 import * as profileServices from "./services/profileServices"
-import Landing from './pages/Landing/Landing';
-import Trending from './pages/Trending/Trending';
+import * as userUtils from "./utils/userUtils"
+
+// Components
 import Nav from './components/Nav/Nav';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
-import * as userUtils from "./utils/userUtils"
-import Profile from './pages/Profile/Profile';
+import TablatureEditorPLUS from './pages/TablatureEditorPLUS/TablatureEditorPLUS';
 
+// Pages
+import Landing from './pages/Landing/Landing';
+import Trending from './pages/Trending/Trending';
+import Profile from './pages/Profile/Profile';
 
 const UserContext = createContext();
 
 function App() {
 
-    // User needs profile info attached to it
-    const [user, setUser] = useState(null)
-    let navigate = useNavigate()
+    const [user, setUser] = useState(null); // Cognito User object that also holds profile data from MongoDB
+    let navigate = useNavigate();
 
     // TODO ---
-    const updateTabInUsersTablature = () => console.log('updateTabInUsersTablature')
-    const addTabToUsersTablature = () => console.log('addTabToUsersTablature')
-    const removeTabFromUsersTablature = () => console.log('removeTabFromUsersTablature')
+    const updateTabInUsersTablature = () => console.log('updateTabInUsersTablature');
+    const addTabToUsersTablature = () => console.log('addTabToUsersTablature');
+    const removeTabFromUsersTablature = () => console.log('removeTabFromUsersTablature');
     // TODO ---
 
-    // Check if there is a user session in local storage
+    /**
+     * Returns session data from local storage if it exists.
+     * @return {Object} session data
+     */
     const getUserSessionFromCognito = useCallback(()=> {
         const getSession = async () => {
             return await new Promise((resolve, reject) => {
@@ -45,10 +51,16 @@ function App() {
             });
         }
 
-        return getSession()
+        return getSession();
 
     }, [user]);
 
+    /**
+     * Checks provided username and password against the Cognito user pool.
+     * Sets 'user' state and navigates to the users profile page on successful authentication.
+     * @param {string} Username 
+     * @param {string} Password 
+     */
     const authenticate = async (Username, Password) => {
         return await new Promise((resolve, reject) => {
             // 1. Create a user object with email and pool info
@@ -83,33 +95,40 @@ function App() {
         });
     };
 
+    /**
+     * Logout user from the user pool and remove session data from local storage.
+     */
     const logout = () => {
         if(user) {
             user.signOut();
             setUser(null);
             navigate('/login')
         }
-    }
+    };
 
+    /**
+     * Handle changes to user state.
+     */
     useEffect(() => {
 
+        // If user is not set, check if there is user data in session storage. This allows users to return to the app without having to log back in each time.
         if(!user) {
-            const userFromPool = UserPool.getCurrentUser()
-            setUser(userFromPool)
+            const userFromPool = UserPool.getCurrentUser();
+            setUser(userFromPool);
         }
         
-        if(user) {
-            const idToken = userUtils.getIdTokenFromUser(user)
-            profileServices.getProfileOfLoggedInUser(user.username, idToken)
+        // Fetch Profile from the backend if it doesn't exist in user state. Profile is the MongoDB Document associated with the user.
+        if(user && !user.profile) {
+            const userFromPool = UserPool.getCurrentUser();
+            const idToken = userUtils.getIdTokenFromUser(userFromPool);
+            profileServices.getProfileOfLoggedInUser(userFromPool.username, idToken)
             .then( (response)=> {
-                const profile = response.profile
-                setUser( prev => {
-                    return { profile, ...prev }
-                })
-            })
-
+                const profile = response.profile;
+                userFromPool['profile'] = profile;
+                setUser( userFromPool );
+            });
         }
-    }, [user])
+    }, [user]);
 
 
   return (
