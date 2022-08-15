04824ee6d8f4db1fb3757c807d18eb274f4457c0
RMo <morici23@gmail.com>
2022-08-15 02:00:14 -0400

    Users can view a single tablature in the content area when hitting the /tablature/:tabId route.

--- src/components/SingleTablatureContent/SingleTablatureContent.js
+++ src/components/SingleTablatureContent/SingleTablatureContent.js
@@ -1,7 +1,31 @@
-const SingleTablatureContent = () => {
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import * as tablatureServices from '../../services/tablatureServices';
+import { CircularProgress } from '@mui/material';
+import TablatureCard from "../TablatureCard/TablatureCard"
+
+const SingleTablatureContent = (props) => {
+    const [tablature, setTablature] = useState(null)
+    const { tabId } = useParams()
+
+    useEffect(() => {
+        tablatureServices.getTablatureById(tabId)
+        .then( res => {
+            console.log(res)
+            setTablature(res.tablature)
+        })
+    }, [tabId])
+
     return (
         <>
-            singleTablaturecontent
+            {tablature === null ?
+                <CircularProgress />
+            :
+                <TablatureCard 
+                    tablature={tablature}
+                    nameOfOwner={tablature.owner.preferredUsername}
+                />
+            }
         </>
     );
 }
