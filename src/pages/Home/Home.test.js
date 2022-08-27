e5855dde5f6ccd66b331d1592aff6dc47f262ee8
RMo <morici23@gmail.com>
2022-08-26 20:43:23 -0400

    Improve Home tests by abstracting common processes to functions. This improves readability and maintainability.

--- src/pages/Home/Home.test.js
+++ src/pages/Home/Home.test.js
@@ -5,11 +5,27 @@ import userEvent from "@testing-library/user-event";
 import React from 'react'
 import Home from "./Home";
 
-test('Home renders trending content area on initial load', async ()=> {
-  render(<Home />, {wrapper: BrowserRouter})
+const testIfTrendingContentIsInTheDocument = () => {
   expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
   expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
   expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+}
+
+const testIfLoginFormIsInTheDocument = () => {
+  expect(screen.getByTestId('LoginForm')).toBeInTheDocument()
+  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
+  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+}
+
+const testIfProfileContentIsInTheDocument = () => {
+  expect(screen.getByTestId('ProfileContent')).toBeInTheDocument()
+  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
+  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
+}
+
+test('Home renders trending content area on initial load', async ()=> {
+  render(<Home />, {wrapper: BrowserRouter})
+  testIfTrendingContentIsInTheDocument()
 })
 
 test('Home renders OfficialNavPlus on intial load', async ()=> {
@@ -25,27 +41,21 @@ test('Home redirects non users to the login page when clicking the Collection bu
   render(<Home />, {wrapper: BrowserRouter})
   const user = userEvent.setup()
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('LoginForm')).toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  testIfLoginFormIsInTheDocument()
 })
 
 test('Home shows non users the trending content when clicking the Latest button', async ()=> {
   render(<Home />, {wrapper: BrowserRouter})
   const user = userEvent.setup()
   await user.click(screen.getByText(/latest/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
 })
 
 test('Home shows non users the trending content when clicking the Trending button', async ()=> {
   render(<Home />, {wrapper: BrowserRouter})
   const user = userEvent.setup()
   await user.click(screen.getByText(/trending/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
 })
 
 const customRender = (ui, {providerProps}) => {
@@ -65,14 +75,11 @@ test('Home shows users their profile content when clicking the Collections butto
       }
     }
   }
-
   customRender(<Home />, {providerProps})
-
   const user = userEvent.setup()
+
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('ProfileContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
+  testIfProfileContentIsInTheDocument()
 })
 
 test('Home shows users trending content when clicking the Trending button', async ()=> {
@@ -85,14 +92,11 @@ test('Home shows users trending content when clicking the Trending button', asyn
       }
     }
   }
-
   customRender(<Home />, {providerProps})
-
   const user = userEvent.setup()
+
   await user.click(screen.getByText(/trending/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
 })
 
 test('Home shows users trending content when clicking the Latest button', async ()=> {
@@ -105,16 +109,12 @@ test('Home shows users trending content when clicking the Latest button', async 
       }
     }
   }
-
   customRender(<Home />, {providerProps})
-
   const user = userEvent.setup()
-  await user.click(screen.getByText(/latest/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-})
 
+  await user.click(screen.getByText(/latest/i))
+  testIfTrendingContentIsInTheDocument()
+})
 
 test('Content area properly swaps between ProfileContent, TrendingContent, and LoginForm for users who are logged in', async ()=> {
   const providerProps = {
@@ -126,41 +126,28 @@ test('Content area properly swaps between ProfileContent, TrendingContent, and L
       }
     }
   }
-
   customRender(<Home />, {providerProps})
-
   const user = userEvent.setup()
 
   // Initial load
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  expect(screen.getByTestId('OfficialNavPlus')).toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
 
   // Test profile link
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('ProfileContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-
+  testIfProfileContentIsInTheDocument()
 
   // Test trending link
   await user.click(screen.getByText(/trending/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-
+  testIfTrendingContentIsInTheDocument()
 
   // Test profile link again to swap content are
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('ProfileContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
+  testIfProfileContentIsInTheDocument()
 
   // Test trending link
   await user.click(screen.getByText(/latest/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
   
 })
 
@@ -170,34 +157,23 @@ test('Content area properly swaps between ProfileContent, TrendingContent, and L
   const user = userEvent.setup()
   
   // Initial load
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
+  expect(screen.getByTestId('OfficialNavPlus')).toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
 
   // Test profile link
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('LoginForm')).toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-
+  testIfLoginFormIsInTheDocument()
 
   // Test trending link
   await user.click(screen.getByText(/trending/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-
+  testIfTrendingContentIsInTheDocument()
 
   // Test profile link
   await user.click(screen.getByText(/collection/i))
-  expect(screen.getByTestId('LoginForm')).toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('TrendingContent')).not.toBeInTheDocument()
+  testIfLoginFormIsInTheDocument()
 
   // Test trending link
   await user.click(screen.getByText(/latest/i))
-  expect(screen.getByTestId('TrendingContent')).toBeInTheDocument()
-  expect(screen.queryByTestId('ProfileContent')).not.toBeInTheDocument()
-  expect(screen.queryByTestId('LoginForm')).not.toBeInTheDocument()
+  testIfTrendingContentIsInTheDocument()
   
 })
