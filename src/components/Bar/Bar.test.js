956fddaf7f2e5175fb109926b1a5f510073bdf83
RMo <morici23@gmail.com>
2022-08-28 11:25:30 -0400

    Write tests for Bar component

--- src/components/Bar/Bar.test.js
+++ src/components/Bar/Bar.test.js
@@ -1,1 +1,12 @@
-test.todo('write tests')
+import { render, screen } from "@testing-library/react"
+import Bar from './Bar'
+
+test('Bar renders both "inputs" and "dashes" text areas', () => {
+  const barData = {
+    inputs: 'test inputs',
+    dashes: 'test dashes',
+  }
+  render(<Bar barData={barData} />)
+  expect(screen.getByText('test inputs')).toBeInTheDocument()
+  expect(screen.getByText('test dashes')).toBeInTheDocument()
+})
