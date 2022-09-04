d3e5e2ffb55036b15fb160621c8a6839e502f294
RMo <morici23@gmail.com>
2022-09-04 00:01:15 -0400

    Write tests for Sidebar, TrendingButton, LatestButton

--- src/pages/Home/components/Sidebar/Sidebar.test.js
+++ src/pages/Home/components/Sidebar/Sidebar.test.js
@@ -1,1 +1,14 @@
-test.todo('write tests')
+import renderer from 'react-test-renderer';
+import Sidebar from './Sidebar'
+import { BrowserRouter } from 'react-router-dom'
+test('renders correctly', ()=> {
+  const component = (
+    <BrowserRouter>
+      <Sidebar />
+    </BrowserRouter>
+  )
+  const tree = renderer
+    .create(component)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
--- src/pages/Home/components/Sidebar/components/TrendingButton/TrendingButton.test.js
+++ src/pages/Home/components/Sidebar/components/TrendingButton/TrendingButton.test.js
@@ -1,3 +1,51 @@
-test.todo('routes to latest on click')
-test.todo('variant changes to "contained" when viewing latest')
-test.todo('variant is "text" when not viewing latest')
+import renderer from 'react-test-renderer';
+import { render, screen } from "@testing-library/react";
+import TrendingButton from './TrendingButton';
+import {BrowserRouter, MemoryRouter} from 'react-router-dom'
+import userEvent from "@testing-library/user-event";
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedNavigate
+}));
+
+test('variant is "text" when not viewing trending', ()=> {
+  const component = (
+    <MemoryRouter initialEntries={['/latest']}>
+      <TrendingButton />
+    </MemoryRouter>
+  )
+  const tree = renderer
+    .create(component)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('variant changes to "contained" when viewing trending', () => {
+
+  const component = (
+    <MemoryRouter initialEntries={['/trending']}>
+      <TrendingButton />
+    </MemoryRouter>
+  )
+
+  const tree = renderer
+  .create(component)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('routes to trending on click', async ()=> {
+  const component = (
+    <BrowserRouter>
+      <TrendingButton />
+    </BrowserRouter>
+  )
+
+  render(component)
+  const user = userEvent.setup()
+  await user.click(screen.getByRole('button'))
+  expect(mockedNavigate).toHaveBeenCalledWith('/trending');
+})
