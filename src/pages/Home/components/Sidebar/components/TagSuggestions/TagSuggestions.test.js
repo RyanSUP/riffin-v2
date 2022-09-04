16410446a1e4df7c83df9f80751ece510fbda0dc
RMo <morici23@gmail.com>
2022-09-04 00:12:50 -0400

    Write tests for TagSuggestions

--- src/pages/Home/components/Sidebar/components/TagSuggestions/TagSuggestions.test.js
+++ src/pages/Home/components/Sidebar/components/TagSuggestions/TagSuggestions.test.js
@@ -1,1 +1,20 @@
-test.todo('write tests')
+import renderer from 'react-test-renderer';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import TagSuggestions from './TagSuggestions';
+
+const mockAddTag = jest.fn(tag => tag)
+
+test('renders correctly', () => {
+  const tree = renderer
+  .create(<TagSuggestions />)
+  .toJSON();
+  expect(tree).toMatchSnapshot();
+})
+
+test('clicking a button passes the tag to the onClick function', async ()=> {
+  render(<TagSuggestions addTag={mockAddTag} />)
+  const user = userEvent.setup()
+  await user.click(screen.getByText('Tasters'))
+  expect(mockAddTag).toHaveBeenCalledWith('Tasters');
+})
