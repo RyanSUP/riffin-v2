118f908d7b6d83c38c2f0c170240fd0f691a850c
Tim Crisp <timothycrisp@gmail.com>
2022-09-14 15:47:58 -0500

    add create buttons directory with create bass tab and create guitar tab buttons for sidebar

---
+++ src/pages/Home/components/Sidebar/components/CreateButtons/CreateGuitarTabButton/CreateGuitarTabButton.js
@@ -0,0 +1,33 @@
+// Components / hooks
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState, useContext } from "react";
+import { UserContext } from "containers/CognitoUserProvider/CognitoUserProvider";
+
+// MUI
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { Button } from "@mui/material";
+
+const CreateGuitarTabButton = () => {
+  const [variant, setVariant] = useState("text")
+  const navigate = useNavigate()
+  const location = useLocation()
+  const { user } = useContext(UserContext)
+  const handleClick = () => {
+    user ? navigate(`/new`) : navigate('/login')
+  }
+
+  return (
+    <Button
+      startIcon={<AddCircleIcon />}
+      onClick={handleClick}
+      sx={{justifyContent: 'left', pl: '16px', my: 1}}
+      disableElevation
+      variant={variant}
+    >
+      Add Guitar Tab
+    </Button>
+  )
+
+}
+
+export default CreateGuitarTabButton
