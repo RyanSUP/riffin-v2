4fd5ed1c8fd4222bca24857dcf1ab7c6f2405c5a
Tim Crisp <timothycrisp@gmail.com>
2022-09-16 15:05:17 -0500

    remove trending content routes

--- src/pages/Home/components/ContentRoutes/ContentRoutes.js
+++ src/pages/Home/components/ContentRoutes/ContentRoutes.js
@@ -1,6 +1,5 @@
 // Components / hooks
 import { Route, Routes, Navigate } from "react-router-dom";
-import TrendingContent from "../TrendingContent/TrendingContent";
 import ProfileContent from "../ProfileContent/ProfileContent";
 import LoginSignupForm from "../LoginSignupForm/LoginSignupForm";
 import Editor from "../Editor/Editor";
@@ -8,14 +7,11 @@ import Editor from "../Editor/Editor";
 const ContentRoutes = (props) => {
   return (
     <Routes>
-      <Route path="/login" element={<LoginSignupForm />} />
-      <Route path="/latest" element={<TrendingContent />} />
-      <Route path="/trending" element={<TrendingContent />} />
+      <Route path="/login" element={<LoginSignupForm />} />      
       <Route
         path="/profile/:cognitoUsername"
         element={<ProfileContent />}
-      />
-      <Route path="/tablature/:tabId" element={<TrendingContent />} />
+      />      
       <Route path="/new" element={
         <Editor tags={props.tags} />
       }/>
