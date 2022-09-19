bf65ddddd8bc75dbd27c4ce40e898e38ae6bc15a
dcollis92 <davidphilipcollis@gmail.com>
2022-09-19 13:47:29 -0500

    Protected Routes component stub up

---
+++ src/pages/Home/components/ProtectedRoute/ProtectedRoute.js
@@ -0,0 +1,12 @@
+// import { Navigate } from "react-router-dom";
+
+// const ProtectedRoute = ({ user, children }) => {
+//   if (!user) {
+//     return <Navigate to="/" />;
+//   }
+//   return children;
+// };
+
+// export default ProtectedRoute;
+
+// TODO Component stubbed up, need to adjust per current user info population
