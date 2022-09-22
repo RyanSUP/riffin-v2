fa00eb040adc62386df13c3f59331d48dba2ece1
David Collis <95148980+dcollis92@users.noreply.github.com>
2022-09-22 11:54:27 -0500

    remove breakpoint config

--- src/Theme.js
+++ src/Theme.js
@@ -1,14 +1,6 @@
 import { createTheme } from "@mui/material/styles";
 
 const darkTheme = createTheme({
-  breakpoints: {
-    values: {
-      mobile: 0,
-      tablet: 640,
-      laptop: 1024,
-      desktop: 1200,
-    },
-  },
   palette: {
     mode: "dark",
     background: {
