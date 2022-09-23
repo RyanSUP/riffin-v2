5baaacbf4d1a20a887f2bbaef3b6b7b972bc0664
RMo <morici23@gmail.com>
2022-09-23 01:08:14 -0400

    Mockup of logo and mobile responsive logo

--- src/pages/Home/components/HeaderLogo/HeaderLogo.js
+++ src/pages/Home/components/HeaderLogo/HeaderLogo.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { useTheme } from "@mui/material/styles";
+import { Box } from "@mui/material";
+import { useMediaQuery } from "@mui/material";
+// Images
+import logo from "../../../../assets/images/riffin_logo.png";
+import mobileLogo from "../../../../assets/images/riffin_mobile_logo.png";
 
 const HeaderLogo = () => {
   const theme = useTheme();
-  return <h1 style={theme.typography.logo}>Riffin</h1>;
+  const onlySmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
+
+  return (
+    <>
+      { onlySmallScreen ?
+          <img src={mobileLogo} alt="logo" style={{width: "45px"}}/>
+        :
+          <Box sx={{display: "flex"}}>
+            <img src={logo} alt="logo" style={{width: "45px"}}/>
+            <h1 style={theme.typography.logo}>Riffin</h1>
+          </Box>
+      }
+    </>
+  );
 };
 
 export default HeaderLogo;
