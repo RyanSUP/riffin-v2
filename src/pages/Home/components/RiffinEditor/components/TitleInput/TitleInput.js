9afddae7b92fe7e909dab6885141d7d56ebe5c70
RMo <morici23@gmail.com>
2022-09-30 23:43:42 -0400

    Document TitleInput

--- src/pages/Home/components/RiffinEditor/components/TitleInput/TitleInput.js
+++ src/pages/Home/components/RiffinEditor/components/TitleInput/TitleInput.js
@@ -1,9 +1,16 @@
+// Components / hooks
 import { RiffinEditorDispatch } from "../../RiffinEditor";
 import { useContext } from "react";
+// MUI
 import { TextField } from "@mui/material";
 
 const TitleInput = () => {
   const dispatcher = useContext(RiffinEditorDispatch);
+
+  /**
+   * Dispatches an action to update the tablature title when the user types in the TextField.
+   * @param {Object} event - the event being fired. Needed so it can be prevented.
+   */
   const handleChange = (event) => {
     event.preventDefault();
     const action = {
@@ -12,6 +19,7 @@ const TitleInput = () => {
     }
     dispatcher(action);
   };
+
   return (
     <TextField
       id="standard-basic" 
