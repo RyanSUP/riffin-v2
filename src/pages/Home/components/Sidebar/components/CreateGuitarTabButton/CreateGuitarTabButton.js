66850ec2f8fd299cc4bafbda0240b76a47c916a7
dcollis92 <davidphilipcollis@gmail.com>
2022-10-04 13:34:53 -0500

    button padding adjustment fix

--- src/pages/Home/components/Sidebar/components/CreateGuitarTabButton/CreateGuitarTabButton.js
+++ src/pages/Home/components/Sidebar/components/CreateGuitarTabButton/CreateGuitarTabButton.js
@@ -4,44 +4,49 @@ import { useContext, useState, useEffect } from "react";
 import { UserContext } from "containers/CognitoUserProvider/CognitoUserProvider";
 import { TagContext } from "containers/TagProvider/TagProvider";
 // MUI
-import AddCircleIcon from '@mui/icons-material/AddCircle';
-import Button from '@mui/material/Button';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import Button from "@mui/material/Button";
 
-const CreateGuitarTabButton = () => {  
-  const [variant, setVariant] = useState("text")
-  const navigate = useNavigate()
-  const location = useLocation()
-  const { user } = useContext(UserContext)
+const CreateGuitarTabButton = () => {
+  const [variant, setVariant] = useState("text");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const { user } = useContext(UserContext);
   const { clearTags } = useContext(TagContext);
-  
+
   const handleClick = () => {
     clearTags();
-    user ? navigate(`/new/guitar`) : navigate('/login')
-  }
+    user ? navigate(`/new/guitar`) : navigate("/login");
+  };
 
-  useEffect(()=> {
-    const urlData = location.pathname.split('/')
-    const path = urlData[2]
-    if(path === 'guitar') {
-      setVariant("contained")
+  useEffect(() => {
+    const urlData = location.pathname.split("/");
+    const path = urlData[2];
+    if (path === "guitar") {
+      setVariant("contained");
     } else {
-      setVariant("text")
+      setVariant("text");
     }
-  }, [location, user])
-  
+  }, [location, user]);
+
   return (
     <Button
       startIcon={<AddCircleIcon />}
       onClick={handleClick}
-      sx={{fontWeight: "bold", justifyContent: 'left', my: 0.5, textTransform: 'none'}}
+      sx={{
+        fontWeight: "bold",
+        justifyContent: "left",
+        my: 0.5,
+        textTransform: "none",
+        padding: 1.5,
+      }}
       disableElevation
       variant={variant}
       size="large"
     >
       Guitar tab
     </Button>
-  )
+  );
+};
 
-}
-
-export default CreateGuitarTabButton
+export default CreateGuitarTabButton;
