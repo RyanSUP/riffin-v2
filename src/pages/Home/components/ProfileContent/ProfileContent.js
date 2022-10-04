939d8e68d9d0f8b882a210c966273df26d0923d1
RMo <morici23@gmail.com>
2022-10-03 22:39:03 -0400

    Write functionality to filter tabs based on tags when in the profile page.

--- src/pages/Home/components/ProfileContent/ProfileContent.js
+++ src/pages/Home/components/ProfileContent/ProfileContent.js
@@ -1,29 +1,50 @@
 // Components / hooks
-import { useEffect, useContext } from "react";
+import { useEffect, useContext, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { UserContext } from "containers/CognitoUserProvider/CognitoUserProvider";
 import { TablatureContext } from "containers/TablatureProvider/TablatureProvider";
+import { TagContext } from "containers/TagProvider/TagProvider";
 import CardGrid from "components/CardGrid/CardGrid";
-
 import LoadingPlaceholder from "containers/LoadingPlaceholder/LoadingPlaceholder";
 
 const ProfileContent = () => {
   const { cognitoUsername } = useParams();
-  const { user, userIsLoading } = useContext(UserContext);
-  const { usersTablature } = useContext(TablatureContext);
+  const { tags } = useContext(TagContext);
+  const { user } = useContext(UserContext);
+  const { usersTablature, tablatureIsLoading } = useContext(TablatureContext);
+  const [tablatureOnPage, setTablatureOnPage] = useState(null);
   const navigate = useNavigate();
 
   // TODO Handle nav for non users
   useEffect(() => {
     if(user && cognitoUsername && (user.username !== cognitoUsername)) {
-      navigate(`/profile/${user.username}`)
+      navigate(`/profile/${user.username}`);
     }
   }, [cognitoUsername, user, navigate]);
 
+  useEffect(() => {
+    if(tablatureIsLoading === false && tablatureOnPage === null) {
+      setTablatureOnPage(usersTablature);
+    }
+  }, [tablatureOnPage, usersTablature, tablatureIsLoading]);
+
+  useEffect(() => {
+    if(tags) {
+      if(tags.length === 0) {
+        setTablatureOnPage(usersTablature);
+      } else {
+        const tablatureWithMatchingTags = usersTablature.filter((tablature) => {
+          return tags.every((tag) => tablature.tags.includes(tag));
+        });
+        setTablatureOnPage(tablatureWithMatchingTags);
+      }
+    }
+  }, [tags, usersTablature]);
+
   return (
-    <LoadingPlaceholder isLoading={userIsLoading}>
+    <LoadingPlaceholder isLoading={tablatureOnPage === null}>
       <CardGrid 
-        tablature={usersTablature}
+        tablature={tablatureOnPage}
       />
     </LoadingPlaceholder>
   );
