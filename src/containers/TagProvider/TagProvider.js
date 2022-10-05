130c49fe68148a863ec40c0e403c32ae1c199d24
RMo <morici23@gmail.com>
2022-10-05 14:34:25 -0400

    Cleanup depreciated functionality in TagContext

--- src/containers/TagProvider/TagProvider.js
+++ src/containers/TagProvider/TagProvider.js
@@ -6,22 +6,8 @@ const TagContext = createContext([]);
 const TagProvider = (props) => {
   const [tags, setTags] = useState([])
 
-  const addTag = (tag) => {
-    if(tags.includes(tag) || tag === '' || tag === ' ') {
-      return
-    }
-    setTags((prev)=> [...prev, tag])
-  }
-
-  const deleteTag = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete))
-
-  const clearTags = () => setTags([])
-
   return (
     <TagContext.Provider value={{
-      addTag,
-      deleteTag,
-      clearTags,
       setTags,
       tags
     }}>
