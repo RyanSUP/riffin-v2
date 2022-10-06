505682aa8a2798765415868346b5ca4ded874b5f
RMo <morici23@gmail.com>
2022-10-06 10:15:09 -0400

    Update tests.

--- src/components/ReadonlyTablature/ReadonlyTablature.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import ReadonlyTablature from './ReadonlyTablature'
-import { sampleTablatureBlock } from "utils/TestUtils/TestUtils" 
-
-test('renders both "inputs" and "dashes" text areas', () => {
-  render(<ReadonlyTablature blockData={sampleTablatureBlock} />)
-  expect(screen.getByText(sampleTablatureBlock.inputs)).toBeInTheDocument()
-  expect(screen.getByText(sampleTablatureBlock.dashes)).toBeInTheDocument()
-})
