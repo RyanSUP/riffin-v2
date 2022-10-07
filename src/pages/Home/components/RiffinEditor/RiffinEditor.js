500062dd7ee29ebcec26897414c8f15b1bf0d363
RMo <morici23@gmail.com>
2022-10-07 16:47:10 -0400

    Cleanup

--- src/pages/Home/components/RiffinEditor/RiffinEditor.js
+++ src/pages/Home/components/RiffinEditor/RiffinEditor.js
@@ -7,46 +7,46 @@ import TablatureBlock from "./components/TablatureBlock/TablatureBlock";
 import { useContext, useState } from "react";
 import { RiffinEditorDispatch } from "./RiffinProvider";
 // MUI
-import { Box, Button, Grid, Switch } from "@mui/material";
+import { Box, Button, Grid } from "@mui/material";
 import BlockContent from "components/Card/components/BlockContent/BlockContent";
 const RiffinEditor = () => {
   const { editor } = useContext(RiffinEditorDispatch);
   const [preview, setPreview] = useState(false);
   return (
     <>
-        <Grid container rowSpacing={2} columnSpacing={4} sx={{alignItems: "end"}}>
-          <Grid item>
-            <TitleInput />
-          </Grid>
-          <Grid item>
-            <SaveTabButton />
-          </Grid>
-          {editor.tablature._id &&
-            <Grid item>
-              <DeleteTabButton />
-            </Grid>
-          } 
-          <Grid item>
-            <Button variant="outlined" onClick={() => setPreview(prev => !prev)}>
-              {preview ? "Edit" : "Preview"}
-            </Button>
-          </Grid>
+      <Grid container rowSpacing={2} columnSpacing={4} sx={{alignItems: "end"}}>
+        <Grid item>
+          <TitleInput />
         </Grid>
-        {preview
-          ?
-            <Box sx={{my:4}}>
-              <BlockContent blocks={editor.tablature.blocks} numberOfStrings={editor.tablature.numberOfStrings}/>
+        <Grid item>
+          <SaveTabButton />
+        </Grid>
+        {editor.tablature._id &&
+          <Grid item>
+            <DeleteTabButton />
+          </Grid>
+        } 
+        <Grid item>
+          <Button variant="outlined" onClick={() => setPreview(prev => !prev)}>
+            {preview ? "Edit" : "Preview"}
+          </Button>
+        </Grid>
+      </Grid>
+      {preview
+        ?
+          <Box sx={{my:4}}>
+            <BlockContent blocks={editor.tablature.blocks} numberOfStrings={editor.tablature.numberOfStrings}/>
+          </Box>
+        :
+          <>
+          {editor.tablature.blocks.map((block, i) => (
+            <Box sx={{my: 4}} key={i}>
+              <TablatureBlock key={i} index={i} block={block} />
             </Box>
-          :
-           <>
-            {editor.tablature.blocks.map((block, i) => (
-              <Box sx={{my: 4}} key={i}>
-                <TablatureBlock key={i} index={i} block={block} />
-              </Box>
-            ))}
-            <AddNewBlockButton />
-           </>
-        }
+          ))}
+          <AddNewBlockButton />
+          </>
+      }
     </>
   );
 }
--- src/pages/Home/components/RiffinEditor/components/TablatureBlock/TablatureBlock.js
+++ src/pages/Home/components/RiffinEditor/components/TablatureBlock/TablatureBlock.js
@@ -2,22 +2,19 @@
 import DashTextarea from "./components/DashTextarea/DashTextarea";
 import InputTextarea from "./components/InputTextarea/InputTextarea";
 import TablatureWrapper from "./components/TablatureWrapper/TablatureWrapper";
-import { useState, useContext } from "react";
+import { useContext } from "react";
 import NoteTextarea from "./components/NoteTextarea/NoteTextarea";
-import OptionsMenu from "./components/OptionsMenu/OptionsMenu";
 import { RiffinEditorDispatch } from "../../RiffinProvider";
 // MUI
 import { Box } from "@mui/material";
 
 /**
- * * TablatureBlock is a section of the screen dedicated to 1 piece of tablature. This component has functionality to show and hide the options menu depending on if the mouse is in the block. Note that a RiffinEditor document can have multiple TablatureBlocks.
+ * * TablatureBlock is a section of the screen dedicated to 1 piece of tablature. Note that a RiffinEditor document can have multiple TablatureBlocks.
  * props: index, block, numberOfStrings
  */
 
 const TablatureBlock = (props) => {
   const { editor } = useContext(RiffinEditorDispatch);
-
-
   return (
     <Box>
       <NoteTextarea label={props.block.label} index={props.index}/>
