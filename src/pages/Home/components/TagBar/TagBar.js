ee48ec667c5a429ad8523478fd62be823bdc0b90
RMo <morici23@gmail.com>
2022-10-18 00:54:41 -0400

    Add Riffin Suggestions to searchbar.

--- src/pages/Home/components/TagBar/TagBar.js
+++ src/pages/Home/components/TagBar/TagBar.js
@@ -6,6 +6,29 @@ import { TablatureContext } from "containers/TablatureProvider/TablatureProvider
 // MUI
 import { Chip, TextField, Autocomplete } from "@mui/material";
 
+const riffinSuggestions = [
+  {
+    label: "Riffin tags",
+    value: "Tasters"
+  },
+  {
+    label: "Riffin tags",
+    value: "Basters"
+  },
+  {
+    label: "Riffin tags",
+    value: "Slick licks"
+  },
+  {
+    label: "Riffin tags",
+    value: "Practice"
+  },
+  {
+    label: "Riffin tags",
+    value: "🤘"
+  },
+]
+
 /**
  * * TagBar is the search / addtag input field with autocomplete suggestions based on the tags found in the User's tablature. All data in the tagbar gets pushed up to the TagProvider so other components can access the data in this input field. The tagbar also pulls data from the TagProvider when loading a tablature to edit, allowing it to be preset with that specific tablature's tags.
  * @returns 
@@ -43,30 +66,25 @@ const TagBar = () => {
    useEffect(() => {
     if(usersTablature) {
       const nameSuggestions = [];
-      const tagMapWithCount = {};
+      const tagSuggestions = [];
+      const tagMap = {};
       usersTablature.forEach((tab) => {
-
         const name = {
           label: "Tablature",
           value: tab.name
         }
         nameSuggestions.push(name);
-
         tab.tags.forEach((tag) => {
-          if(tagMapWithCount[tag]) {
-            tagMapWithCount[tag] = tagMapWithCount[tag] + 1;
-          } else {
-            tagMapWithCount[tag] = 1;
+          if(!tagMap[tag]) {
+            tagMap[tag] = true;
+            tagSuggestions.push({
+              label: "Your tags",
+              value: tag
+            });
           }
         });
       });
-      let tagSuggestions = Object.keys(tagMapWithCount).map((tag) => {
-        return {
-          label: "Tags",
-          value: tag
-        }
-      })
-      setOptions([...tagSuggestions, ...nameSuggestions])
+      setOptions([...tagSuggestions, ...nameSuggestions, ...riffinSuggestions.filter((suggestion) => !tagSuggestions.map(tag => tag.value).includes(suggestion.value))])
     }
   }, [usersTablature]);
 
