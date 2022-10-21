3251fd3c676c2f8ce4a9cc0596dc38c1359cc63d
Tim Crisp <timothycrisp@gmail.com>
2022-10-21 14:10:57 -0500

    create searchTagsMap object and tablature to display set for profile content use effect - this should set us up for a ternary where we can return the tabs in the set

--- src/pages/Home/components/ProfileContent/ProfileContent.js
+++ src/pages/Home/components/ProfileContent/ProfileContent.js
@@ -6,6 +6,7 @@ import { TablatureContext } from "containers/TablatureProvider/TablatureProvider
 import { TagContext } from "containers/TagProvider/TagProvider";
 import LoadingPlaceholder from "containers/LoadingPlaceholder/LoadingPlaceholder";
 import Card from "components/Card/Card";
+import { Sd } from "@mui/icons-material";
 
 const ProfileContent = () => {
   const { cognitoUsername } = useParams();
@@ -16,6 +17,7 @@ const ProfileContent = () => {
   const navigate = useNavigate();
 
   // TODO Handle nav for non users
+
   useEffect(() => {
     if(user && cognitoUsername && (user.username !== cognitoUsername)) {
       navigate(`/profile/${user.username}`);
@@ -24,15 +26,37 @@ const ProfileContent = () => {
 
   useEffect(() => {
     if(tagsInSearchbar) {
+      
       if(tagsInSearchbar.length === 0) {
         setTablatureOnPage(usersTablature);
       } else {
-        // Get all tablature objects whose tags or name match all tags in the searchbar.
-        const tablatureWithMatchingTags = usersTablature.filter((tablature) => {
-          return tagsInSearchbar.every((tag) => tablature.tags.map((tag) => tag.toLowerCase()).includes(tag.toLowerCase()) || tablature.name.toLowerCase() === tag.toLowerCase());
-        });
-        setTablatureOnPage(tablatureWithMatchingTags);
-      }
+          const searchTagsMap = {}
+          let tablatureToDisplay = new Set()
+
+          for (let tag of tagsInSearchbar) {
+            if (!searchTagsMap[tag]) {
+              searchTagsMap[tag] = true
+            }
+          }        
+          
+          usersTablature.forEach((tab) => {
+            let name = tab.name
+            let tags = tab.tags
+            
+            for (let tag of tags) {
+              if (searchTagsMap[tag]) {
+                tablatureToDisplay.add(tag)
+              }
+            }
+          })
+          console.log(searchTagsMap, tablatureToDisplay)
+      }        
+      // Get all tablature objects whose tags or name match all tags in the searchbar.
+      const tablatureWithMatchingTags = usersTablature.filter((tablature) => {
+        return tagsInSearchbar.every((tag) => tablature.tags.map((tag) => tag.toLowerCase()).includes(tag.toLowerCase()) || tablature.name.toLowerCase() === tag.toLowerCase());
+      });
+      setTablatureOnPage(tablatureWithMatchingTags);
+      
     }
   }, [tagsInSearchbar, usersTablature]);
 
@@ -46,3 +70,41 @@ const ProfileContent = () => {
 };
 
 export default ProfileContent;
+
+// useEffect(() => {
+  //   if(user && cognitoUsername && (user.username !== cognitoUsername)) {
+  //     navigate(`/profile/${user.username}`);
+  //   }
+  // }, [cognitoUsername, user, navigate]);
+
+  // useEffect(() => {
+  //   if(tagsInSearchbar) {
+      
+      
+  //     if(tagsInSearchbar.length === 0) {        
+  //       setTablatureOnPage(usersTablature);
+      
+  //     } else {
+  //       const nameMap = {}
+  //       const tagMap = {}
+  //       const tablatureToDisplay = []
+  //       usersTablature.forEach((tab) => {
+  //       const name = tab.name
+  //       const tags = tab.tags
+  //       let meetsCriteria = true
+  //       for (let tag of tags) {
+  //         if (!tagMap[tag]) {
+  //           tagMap[tag] = true
+  //         }
+  //       }
+  //       for (let tag of tagsInSearchbar) {
+  //         if (!tagMap[tag]) {
+            
+  //         }
+  //       }
+
+  //     })
+  //     console.log(tagMap)
+  //     }
+  //   }
+  // }, [tagsInSearchbar, usersTablature]);
