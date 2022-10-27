e66101a6ccc55b7409ed881438a5ed2e31b90ecd
RMo <morici23@gmail.com>
2022-10-27 18:00:34 -0400

    Resolve #56. A user is prompted to confirm deleting a tab.

--- src/pages/Home/components/RiffinEditor/components/DeleteTabButton/DeleteTabButton.js
+++ src/pages/Home/components/RiffinEditor/components/DeleteTabButton/DeleteTabButton.js
@@ -1,5 +1,5 @@
 // Components / hooks
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { TablatureContext } from "containers/TablatureProvider/TablatureProvider";
 import { UserContext } from "containers/CognitoUserProvider/CognitoUserProvider";
@@ -9,7 +9,7 @@ import { useSnackbar } from "notistack";
 import * as tablatureServices from "services/tablatureServices";
 import { getIdTokenFromUser } from "utils/userUtils";
 // MUI
-import { Button } from "@mui/material";
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText } from "@mui/material";
 import DeleteIcon from '@mui/icons-material/Delete';
 
 /**
@@ -23,7 +23,17 @@ const DeleteTabButton = (props) => {
   const { user } = useContext(UserContext);
   const { editor } = useContext(RiffinEditorDispatch);
   const { deleteFromUsersTablature } = useContext(TablatureContext);
-  
+  const [open, setOpen] = useState(false);
+
+  /**
+   * This closes the delete confirmation dialog. If 'delete' is passed as an action, the logic to delete the tab is carried out. 
+   * @param {String} action 
+   */
+  const handleDialogClose = (action) => {
+    if(action === 'delete') handleDelete();
+    setOpen(false);
+  }
+
   /**
    * Sends a request to delete the tab on the backend and shows a snackbar depending on status of deleting
    */
@@ -41,26 +51,46 @@ const DeleteTabButton = (props) => {
   /**
    * Sends a request to the backend to delete the tablature, then navigates the user to their profile. If a user does not own the tab they will just be redirected to their profile (this is a defensive catch as a non-owner should never have a access to editing a tab they don't own).
    */
-  const handleDelete = () => {
-    console.log('entering delete', editor.tablature)
+  const handleDelete = async () => {
     if(editor.tablature.owner.user === user.username) {
       console.log('delete if statement is true');
       deleteFromUsersTablature(editor.tablature);
-      dispatchDeleteRequestToBackend()
+      dispatchDeleteRequestToBackend();
     }
     navigate(`/profile/${user.username}`);
   };
 
   return (
-    <Button 
-      variant="outlined" 
-      onClick={handleDelete}
-      disabled={props.disabled}
-      color="error"
-      startIcon={<DeleteIcon />}
-    >
-      Delete
-    </Button>
+    <>    
+      <Button 
+        variant="outlined" 
+        onClick={()=> setOpen(true)}
+        disabled={props.disabled}
+        color="error"
+        startIcon={<DeleteIcon />}
+      >
+        Delete
+      </Button>
+
+      <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      >
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete this tab?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleDialogClose}>Cancel</Button>
+          <Button color="error" onClick={() => handleDialogClose('delete')} autoFocus>
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 };
  
