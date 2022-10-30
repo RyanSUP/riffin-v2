a43b0030cc66a05d9dbf4f7da813e1348df56084
RMo <morici23@gmail.com>
2022-10-30 14:49:57 -0400

    Resolve #88 by implementing message components. A message will display if the user has no tablature, this message takes priority over the filter message. Filter message displays a 'no matches found' message when there are no filter results.

---
+++ src/pages/Home/components/ProfileContent/components/NoMatchesMessage.js
@@ -0,0 +1,11 @@
+import { Box, Typography } from "@mui/material";
+
+const NoMatchesMessage = () => {
+  return (
+    <Box sx={{margin: "25px auto"}}>
+      <Typography>No matches found</Typography>
+    </Box>
+  );
+}
+ 
+export default NoMatchesMessage;
