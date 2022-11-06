2e1b48321c8b0a20d5e3c91732caab3a08f85534
RMo <morici23@gmail.com>
2022-11-06 14:46:47 -0500

    Refactor forms to use react-form library. It doesn't help much at the moment but it lays the groundwork for a better sign on experience.

--- src/pages/Home/components/LoginSignupForm/components/SignupForm/SignupForm.js
+++ src/pages/Home/components/LoginSignupForm/components/SignupForm/SignupForm.js
@@ -5,48 +5,53 @@ import UserPool from "utils/UserPool";
 import { UserContext } from "containers/CognitoUserProvider/CognitoUserProvider";
 import { useState, useContext } from "react";
 import { Button, TextField, Stack } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 const SignupForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [errorMessage, setErrorMessage] = useState();
   const { authenticate } = useContext(UserContext);
 
   /**
    * handles the sign up form submit
    * @param {Object} event
    */
-  const onSubmit = (event) => {
-    event.preventDefault();
-    UserPool.signUp(email, password, [], null, (error, data) => {
+  const onSubmit = (formData) => {
+    if(formData['Password'] !== formData['Confirm Password']) {
+      console.log('Error!')
+      setErrorMessage("* Password and Confirm Password must match, dude!")
+    }
+    console.log(formData)
+    UserPool.signUp(formData["Email"], formData["Password"], [], null, (error, data) => {
       if (error) {
-        console.error(error);
+        console.dir(error);
+        if(error.code === 'UsernameExistsException') {
+          setErrorMessage('* an account with that email already exists, dude!')
+        } else {
+          setErrorMessage("* Something went wrong, sorry! Try again, dude!")
+        }
       } else {
-        authenticate(email, password, "new user")
+        authenticate(formData["Email"], formData["Password"], "new user")
         .then((user) => {
           console.log("Successfully authenticated ", user)
         })
         .catch((error) => {
           console.error("Failed to sign up: ", error);
+          setErrorMessage('* failed to sign up. Sorry dude!')
         });
       }
     });
   };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={2}>
-        <TextField
-          label="Email"
-          variant="outlined"
-          onChange={(event) => setEmail(event.target.value)}
-          value={email}
-        />
-        <TextField
-          label="Password"
-          variant="outlined"
-          onChange={(event) => setPassword(event.target.value)}
-          value={password}
-        />
+        <TextField required type="email" variant="outlined" placeholder="Email" {...register("Email", {required: true})} />
+        <TextField required type="password" variant="outlined" placeholder="Password" {...register("Password", {required: true})} />
+        <TextField required type="password" variant="outlined" placeholder="Confirm Password" {...register("Confirm Password", {required: true})} />
+        {errorMessage &&
+          <p style={{color: "red"}}>{errorMessage}</p>
+        }
         <Button variant="contained" type="submit">
           <span data-testid="signup-button">Signup</span>
         </Button>
