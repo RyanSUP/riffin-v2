e6928bf2239e4d225b5dc1f298f220fac3da07fe
RMo <morici23@gmail.com>
2022-11-10 14:47:21 -0500

    Add button for 'buy me a coffee'

---
+++ src/pages/Home/components/HeaderLinks/BuyMeACofeeButton/BuyMeACoffeeButton.js
@@ -0,0 +1,9 @@
+const BuyMeACoffeeButton = () => {
+  return (
+    <a href="https://www.buymeacoffee.com/morici23" target="_blank" rel="noreferrer" style={{maxWidth: "160px", height: 0}}>
+      <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" style={{maxWidth: "160px"}} />
+    </a>
+  );
+}
+ 
+export default BuyMeACoffeeButton;
