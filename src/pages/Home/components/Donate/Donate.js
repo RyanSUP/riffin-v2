78f29c006f0cb537afb1067019dac9afe9bea10e
RMo <morici23@gmail.com>
2022-11-10 21:31:57 -0500

    Update developer credits.

--- src/pages/Home/components/Donate/Donate.js
+++ src/pages/Home/components/Donate/Donate.js
@@ -1,28 +1,29 @@
-import { Button, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import { Box, Stack, Divider } from "@mui/material";
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Donate = () => {
   return (
     // TODO This can be maped.
-    <Stack spacing={3}>
-      <Stack spacing={1}>
+    <Stack spacing={3} sx={{textAlign: 'left'}}>
+      <Divider >
         <Typography variant="p">Made by</Typography>
+      </Divider>
+      <Stack spacing={2}>
         <a href="https://www.linkedin.com/in/ryanmorici/" target="_blank" rel="noreferrer" style={{textDecoration: 'none', color: 'white'}}>
           <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
             <Typography variant="h2">Ryan Morici</Typography>
             <LinkedInIcon />
           </Box>
         </a>
-      </Stack>
-      <Stack spacing={1}>
-        <Typography variant="p">with contributions from</Typography>
+        <Typography variant="p">with a little help from my friends</Typography>
         <a href="https://www.linkedin.com/in/davidphilipcollis/" target="_blank" rel="noreferrer" style={{textDecoration: 'none', color: 'white'}}>
           <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
             <Typography variant="h2">Dave Collis</Typography>
             <LinkedInIcon />
           </Box>
         </a>
+        <Typography variant="p">&</Typography>
         <a href="https://www.linkedin.com/in/timcrisp94/" target="_blank" rel="noreferrer" style={{textDecoration: 'none', color: 'white'}}>
           <Box sx={{display: 'flex', justifyContent: 'space-between'}}>
             <Typography variant="h2">Tim Crisp</Typography>
@@ -31,6 +32,7 @@ const Donate = () => {
         </a>
       </Stack>
       <Divider />
+      <Typography variant="p">Be excellent to eachother, and party on dudes!</Typography>
     </Stack>
   );
 }
