5b7b3d1a2a9b76b0cea2d3fa12213e98a7e733cd
RMo <morici23@gmail.com>
2022-12-28 11:30:09 -0500

    Remove unused variables.

--- src/pages/Home/components/TagBar/TagBar.js
+++ src/pages/Home/components/TagBar/TagBar.js
@@ -6,28 +6,6 @@ import { TablatureContext } from "containers/TablatureProvider/TablatureProvider
 // MUI
 import { Chip, TextField, Autocomplete } from "@mui/material";
 
-const riffinOptions = [
-  {
-    label: "Riffin tags",
-    value: "Tasters"
-  },
-  {
-    label: "Riffin tags",
-    value: "Basters"
-  },
-  {
-    label: "Riffin tags",
-    value: "Slick licks"
-  },
-  {
-    label: "Riffin tags",
-    value: "Practice"
-  },
-  {
-    label: "Riffin tags",
-    value: "🤘"
-  },
-]
 
 /**
  * * TagBar is the search / addtag input field with autocomplete suggestions based on the tags found in the User's tablature. All data in the tagbar gets pushed up to the TagProvider so other components can access the data in this input field. The tagbar also pulls data from the TagProvider when loading a tablature to edit, allowing it to be preset with that specific tablature's tags.
